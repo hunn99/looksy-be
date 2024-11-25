ba13555de588f2860212a6ffdfe0db6b2724fd7b
satriaabraarr <satria.abrarr@gmail.com>
2024-11-25 11:29:42 +0700

    [feat] History API

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -3,11 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderDetail extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'order_id',
         'service_id',
     ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\HistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\ApiMiddleware;
@@ -11,12 +12,10 @@ use App\Http\Middleware\ApiMiddleware;
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-// Routes that require authentication
-// Route::middleware(ApiMiddleware::class)->group(function () {
-//     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-// });
-
 Route::middleware(ApiMiddleware::class)->post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:sanctum')->get('/services', [ServiceController::class, 'getServices'])->name('getServices');
 Route::middleware('auth:sanctum')->post('/orders', [OrderController::class, 'createOrder'])->name('createOrder');
+
+Route::middleware('auth:sanctum')->get('/history', [HistoryController::class, 'getHistory'])->name('getHistory');
+Route::middleware('auth:sanctum')->post('/history/cancel/{orderId}', [HistoryController::class, 'cancelOrder'])->name('cancelOrder');
