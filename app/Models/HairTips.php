228154d93e3f82c85cd5cdd7398be3cccfe42b33
hunn99 <dennymalikibrahim@gmail.com>
2024-12-05 15:56:55 +0700

    [chore] Delete command code

--- app/Models/HairTips.php
+++ app/Models/HairTips.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
 
 class HairTips extends Model
 {
@@ -12,4 +14,17 @@ class HairTips extends Model
         'description',
         'photo',
     ];
+
+    // public function getPhotoAttribute($value)
+    // {
+    //     if (filter_var($value, FILTER_VALIDATE_URL)) {
+    //         // Jika sudah berupa URL lengkap, kembalikan langsung
+    //         return $value;
+    //     }
+
+    //     // Jika path relatif, buat URL lengkap dari disk publik
+    //     return Storage::disk('public')->exists($value)
+    //         ? asset('storage/' . $value)
+    //         : null; // Jika file tidak ditemukan, kembalikan null
+    // }
 }
