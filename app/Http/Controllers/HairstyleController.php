9c4a661ba3782420d2ecd8e582e3cd19e97a42b1
hunn99 <dennymalikibrahim@gmail.com>
2024-12-10 23:56:58 +0700

    [feat] Add hairstyle controller

---
+++ app/Http/Controllers/HairstyleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HairStyle;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class HairstyleController extends Controller
+{
+    public function hairstyleRecommendation(Request $request)
+    {
+        $credentials = $request->only('face_shape');
+
+        // Validasi input
+        $validateData = Validator::make($credentials, [
+            'face_shape' => 'required|string',
+        ]);
+
+        if ($validateData->fails()) {
+            return response()->json(['error' => 'Validation failed', 'messages' => $validateData->errors()], 422);
+        }
+
+        $hairstyles = HairStyle::where('face_shape', '=', $credentials['face_shape'])
+            ->get()
+            ->map(function ($hairstyles) {
+                return [
+                    'hair_style' => $hairstyles->hairstyle,
+                    'face_shape' => $hairstyles->face_shape,
+                    'photo' =>  env('APP_URL') . $hairstyles->photo,
+                    'characteristics' => $hairstyles->characteristics,
+                    'face_suitability' => $hairstyles->faceSuitability,
+                    'maintenance' => $hairstyles->maintenance,
+                    'impression' => $hairstyles->impression,
+                ];
+            });
+
+        return response()->json(['message' => 'Orders retrieved successfully', 'data' => $hairstyles], 200);
+    }
+}
